8a56686ab0e19311f348eaf1455a4cf00a3502f1
Rachid Boukaiba <rachidd@protonmail.com>
2021-06-02 22:41:51 +0200

    added regex parsing for cam numbers and removed greps line numbers, added example and error messages

--- script/findcams.sh
+++ script/findcams.sh
@@ -1,32 +1,40 @@
 #!/bin/bash
 
+example="Example: findcams.sh --name plein"
 if [ -z "$1" ] || [ -z "$2" ]
-then
-        echo "2 arguments required!"
-        exit 1
+  then
+      echo -e "Error: 2 arguments required!\n${example}"
+      exit 1
 fi
 
 if [ "$1" != "--name" ]
-then
-        echo "--name is the only supported option!"
-        exit 1;
+  then
+      echo -e "Error: --name is the only supported option!\n${example}"
+      exit 1
 fi
 
-tail -n +2 ./data/cameras.csv | grep -in "$2" | grep --invert-match "ERROR" | sed 's/:/ | /' | sed 's/;/ | /g'
+cd "$(dirname "${BASH_SOURCE[0]}")"
+regex="CM-([0-9]+)"
+
+tail -n +2 './data/cameras.csv' | grep -i "$2" | grep --invert-match "ERROR" | sed 's/;/ | /g' | while read -r line; do
+  if [[ $line =~ $regex ]]
+      then
+        number="${BASH_REMATCH[1]}"
+        echo "${number} | ${line}"
+  fi
+done
+
+# cd "$(dirname "${BASH_SOURCE[0]}")"
+# set working directory to this scripts location, in order to access files using relative paths
 
 # tail -n +2 ./data/cameras.csv
 # Used to ignore first line of file (csv header)
 
-# grep -in "$2"
-# used to match the name we're searching for with the following options:
-# -i (case insensitive search)
-# -n (adds line numbers)
+# grep -i "$2"
+# searches lines matching the name with case insensitive setting
 
 # grep --invert-match "ERROR"
-# Remove lines containing the word "ERROR"
-
-# sed 's/:/ | /'
-# Replace the line number separator : with | and adds padding
+# Remove malformed entries (lines containing the word "ERROR")
 
 # sed 's/;/ | /g'
 # Turns the csv separator ; into | and adds padding
